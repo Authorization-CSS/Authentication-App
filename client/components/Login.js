6c783c941651fe34e2ec598c6b26922e1fb6ff29
Kyle Parkinson <kparki1130@gmail.com>
2022-08-11 11:59:23 -0400

    Login and SignUp components made & model

---
+++ client/components/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    const { email, password } = this.state;
+    const { handleChange, handleSubmit } = this;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email"> Email </label>
+          <input
+            name="email"
+            type="text"
+            onChange={handleChange}
+            value={email}
+          />
+          <label htmlFor="password"> Password </label>
+          <input
+            name="password"
+            type="text"
+            onChange={handleChange}
+            value={password}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  login: user => dispatch(login(user))
+});
+
+export default connect(null, mapDispatchToProps)(Login);
