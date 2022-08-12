05eff8fc71efd28eac5363aaee7a59889198d19b
Ryan Scoville <ryanscoville29@yahoo.com>
2022-08-12 10:59:55 -0500

    added fourohfour and routes

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
+import FourOhFour from './FourOhFour';
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path='/signup' component={SignUp}></Route>
+                <Route exact path='/login' component={Login}></Route>
                 <Route path='/' component={Login}></Route>
-                <Route exact path='*' component={Login}></Route>
+                <Route exact path='*' component={FourOhFour}></Route>
                 </Switch>
         </Router>
     )
