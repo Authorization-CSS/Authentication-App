5f4b3c0fb3d2c00e69618fda637903c30d3f815b
Ryan Scoville <ryanscoville29@yahoo.com>
2022-08-12 11:53:50 -0500

    added App component

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
 import FourOhFour from './FourOhFour';
+import App from './App';
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path='/signup' component={SignUp}></Route>
-                <Route exact path='/login' component={Login}></Route>
-                <Route path='/' component={Login}></Route>
+                <Route exact path='/login' component={App}></Route>
+                <Route path='/' component={App}></Route>
                 <Route exact path='*' component={FourOhFour}></Route>
                 </Switch>
         </Router>
--- server/db/user.js
+++ server/db/user.js
@@ -26,4 +26,36 @@ const User = db.define("user", {
   }
 });
 
+User.byToken = async(token)=> {
+  try {
+    const user = await User.findByPk(token);
+    if(user){
+      return user;
+    }
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+  catch(ex){
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async({ username, password })=> {
+  const user = await User.findOne({
+    where: {
+      username,
+      password
+    }
+  });
+  if(user){
+    return user.id; 
+  }
+  const error = Error('bad credentials');
+  error.status = 401;
+  throw error;
+};
+
 module.exports = User;
